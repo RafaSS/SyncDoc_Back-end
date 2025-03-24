2731493f586e7a58d5896f774de4f807eb0155e5
Rafael de Souza Silva <77814194+RafaSS@users.noreply.github.com>
2025-03-24 18:20:33 -0300

    fix vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,21 +4,4 @@ import vue from "@vitejs/plugin-vue";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    proxy: {
-      "/api": {
-        target: process.env.VITE_API_BASE_URL,
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
-  build: {
-    outDir: "dist",
-  },
 });
